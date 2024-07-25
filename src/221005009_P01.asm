a2a515a23c547fdecd7c52eeb21f813af428cf2c
Baraka <barbuk.bb@gmail.com>
2024-07-25 14:57:00 +0200

    P01 2024

---
+++ src/221005009_P01.asm
@@ -0,0 +1,67 @@
+;	Author: B Bukanga
+;	Template document
+.386
+.MODEL FLAT ; Flat memory model
+.STACK 4096 ; 4096 bytes
+INCLUDE io.inc
+
+; Exit function
+ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD
+
+; The data section stores all global variables
+.DATA
+    distanceMeters          DWORD   ?
+    distanceKilometers      DWORD   ?
+    stride                  DWORD   ?
+    stepcount               DWORD   ?
+
+
+    strDistanceMeters          BYTE   "The distance in Meters is: ", 0
+    strDistanceKilometers      BYTE   "The distance in Kilomiters is: ", 0
+    strStride                  BYTE   "Enter the Stride in cm: ", 0
+    strStepcount               BYTE   "Enter the Steps: ", 0
+    strNL                      BYTE   10, 0
+
+.CODE
+_start:
+
+    ; getting values for stride
+    INVOKE OutputStr, ADDR strStride
+    INVOKE InputInt
+    mov stride, EAX
+
+    ; getting values for count
+    INVOKE OutputStr, ADDR strStepcount
+    INVOKE InputInt
+    mov stepcount, EAX
+
+    ; (stride * stepcount)
+    mov eax, stride
+    imul eax, stepcount
+
+    ; /100
+    mov ebx, 100
+    cdq
+    idiv ebx
+    mov distanceMeters, eax
+
+    ; (distanceMeters)/1000
+    mov eax, distanceMeters
+    mov ebx, 1000
+    cdq
+    idiv ebx
+    mov distanceKilometers, eax
+
+    ; output string
+    INVOKE OutputStr, ADDR strDistanceMeters
+    INVOKE OutputInt, distanceMeters
+    INVOKE OutputStr, ADDR strNL
+
+    INVOKE OutputStr, ADDR strDistanceKilometers
+    INVOKE OutputInt, distanceKilometers
+    INVOKE OutputStr, ADDR strNL
+
+
+	INVOKE ExitProcess, 0
+Public _start
+END
