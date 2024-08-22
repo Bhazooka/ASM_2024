d707574b4b82bfd8f3b67f4eab20c4290f01e0ae
Baraka <barbuk.bb@gmail.com>
2024-08-22 18:54:37 +0200

    221005009_P04

--- src/Bukanga_B_22100509_CSC03B3_2024_P04/src/221005009_P04.asm
+++ src/Bukanga_B_22100509_CSC03B3_2024_P04/src/221005009_P04.asm
@@ -7,14 +7,14 @@ INCLUDE io.inc
 
 ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD
 
-; Global variables
+; The data section global variables
 .DATA
 ;Constant variables
 HR		        DWORD		3 DUP (?)
 HRF             DWORD       3 DUP (?)
 
-;String Prompts
-strAge          BYTE        "Enter age (in years): ",0
+;User prompts 
+strAge          BYTE        "Enter age: ",0
 strRHR          BYTE        "Enter RHR value: ",0
 strCMul         BYTE        "Enter CMul value: ",0
 strHR           BYTE        "Enter HR value at index ",0
@@ -28,33 +28,39 @@ ClosedBracket   BYTE        "]",0
 OpenBracket     BYTE        "[",0
 SemiColon       BYTE        ":",0
 Comma           BYTE        ",",0
-ContinueCode    BYTE        "User, press 0 to exit or any number to resume:",0
+Restart         BYTE        "User, press 0 to exit or any number to resume:",0
+
 .CODE
 _start:
-	;Create the stack frame for local variables
+	 ;Create the stack frame for local variables
 	push			ebp
 	mov				ebp,esp
 	;Local variables
-	;Age			[ebp-4]
-	;RHR			[ebp-8]
-	;CMul			[ebp-12]
-	sub				esp,12           ;Since we only need space for only 3 variables here
+	;Age			          [ebp-4]
+	;RHR			          [ebp-8]
+	;CMul			          [ebp-12]
+	;(HRi – RHR)              [ebp-16]
+    ;(HRi – RHR) × CMul       [ebp-20]
+	;(220 - age)              [ebp-24]
+	;(220-age)-RHR            [ebp -26]
+	;HRF(i)                   [ebp -30]
+	sub				          esp,30           ;Since we only need space for only 3 variables here
 
 	;Getting input values from the user
 InputLoopStart:
 	INVOKE          OutputStr, ADDR strAge
 	;Storing the age value into stack
-	INVOKE          InputInt               ;Taking the age
-	mov             [ebp-4], eax           ;Storing the age
-    ;Asking for the RHR value from user
+	INVOKE          InputInt               ;ask age
+	mov             [ebp-4], eax           ;Storing age
+    ;Asking for RHR value from user
 	INVOKE          OutputStr, ADDR strRHR
 	INVOKE          InputInt
-	mov             [ebp-8], eax           ;Storing the RHR in
-    ;Asking for the CMul value from the user:
+	mov             [ebp-8], eax           ;Storing the RHR
+    ;Asking CMul value from user:
     INVOKE          OutputStr, ADDR strCMul
 	INVOKE          InputInt
 	mov             [ebp-12] ,eax              ;Storing the CMul value in the right variable
-    ;Asking the user for the arrayHR inputs
+    ;Asking the user for the HRarray inputs
 	mov             ecx,0
 HRArrayLoop:
 	INVOKE			OutputStr, ADDR strHR
@@ -68,65 +74,102 @@ HRArrayLoop:
 	inc             ecx
 	cmp             ecx,3
 	jl              HRArrayLoop
-
-;Displaying the values
-;Printing age
-INVOKE              OutputStr, ADDR AgeValue
-INVOKE              OutputInt, DWORD PTR [ebp-4]
-INVOKE              OutputStr, ADDR EndLine
-;Printing RHR value
-INVOKE              OutputStr, ADDR RHRValue
-INVOKE              OutputInt, DWORD PTR [ebp-8]
-INVOKE              OutputStr, ADDR EndLine
-;Printing CMul value
-INVOKE              OutputStr, ADDR CMulValue
-INVOKE              OutputInt, DWORD PTR [ebp-12]
-INVOKE              OutputStr, ADDR EndLine
-;Printing the HR array values
-INVOKE              OutputStr, ADDR HRValues
-INVOKE              OutputStr, ADDR OpenBracket
+;Firstly accessing the array values for the array HR:
 mov                 ecx, 0
-_OutputHRArrayLoop:
+HRElements:
 	lea				ebx,HR 					;Calculate the memory array offset using the formulae: (ecx * 4) + baseaddress
 	imul			eax,ecx,4                  ;EAX = ECX*4
 	add				ebx,eax                    ;EBX = EBX + EAX, to move to the correct index in the array
-	INVOKE			OutputInt,[ebx]				;Output the array item at the index
+    mov             edx, [ebx]
+
+    ;Substracting:  HR[i]-RHR
+	sub             edx,[ebp-8] 
+    mov             [ebp-16],edx     ;[ebp-16] = edx = HR[i]-RHR
+	mov             edx, DWORD PTR [ebp-12]
+	imul            edx, DWORD PTR [ebp-16]
+    mov             [ebp-20], edx    ;[ebp-20] = (HR[i]-RHR)*Cmul
+	mov             edx, 220
+    sub             edx, [ebp-4]
+	mov            [ebp-24], edx   ;[ebp-24] = (220 - age)
+    mov            edx, [ebp-24]
+	sub            edx, [ebp-8]    ; (220 - age) - RHR
+	mov            [ebp -26], edx  ; [ebp-26] = (220 - age)-RHR
+	cdq
+	mov             eax, [ebp-20]
+	mov             ebx, [ebp -26]
+	div             ebx
+    mov             [ebp -30], eax  ;[ebp -30] has the HRF_i
+	;Storing the array values here:
+	lea				ebx,HRF					;Calculate the memory array offset using the formulae: (ecx * 4) + baseaddress
+	imul			eax,ecx,4                  ;EAX = ECX*4
+	add				ebx,eax                    ;EBX = EBX + EAX, to move to the correct index in the array
+    mov             edx, [ebp -30]
+    mov             [ebx], edx
+	inc             ecx
+	cmp             ecx, 3
+	jl              HRElements
+
+;Displaying the values
+;Printing the age
+INVOKE              OutputStr, ADDR AgeValue
+INVOKE              OutputInt, DWORD PTR [ebp-4]
+INVOKE              OutputStr, ADDR EndLine
+;Printing the RHR value here:
+INVOKE              OutputStr, ADDR RHRValue
+INVOKE              OutputInt, DWORD PTR [ebp-8]
+INVOKE              OutputStr, ADDR EndLine
+;Printing the CMul value here:
+INVOKE              OutputStr, ADDR CMulValue
+INVOKE              OutputInt, DWORD PTR [ebp-12]
+INVOKE              OutputStr, ADDR EndLine
+;Printing the HR array values here:
+INVOKE              OutputStr, ADDR HRValues
+INVOKE              OutputStr, ADDR OpenBracket
+mov                 ecx, 0
+DisplayHRArrayLoop:
+	lea				ebx,HR 					   ;Calculate the memory array offset using the formulae: (ecx * 4) + baseaddress
+	imul			eax,ecx,4                  ;EAX = ECX*4
+	add				ebx,eax                    ;EBX = EBX + EAX, to move to the correct index in the array
+	INVOKE			OutputInt,[ebx]			   ;Output the array item at the index
 	cmp				ecx,2                      ;Comparing the values
-	je				_InsertComma
+	je				AddComma
 	INVOKE			OutputStr, ADDR Comma
-_InsertComma:
+AddComma:
 	inc				ecx
 	cmp 		   	ecx,3 
-	jl				_OutputHRArrayLoop
+	jl				DisplayHRArrayLoop
 	INVOKE			OutputStr, ADDR ClosedBracket
     INVOKE          OutputStr, ADDR EndLine
+
 ;Printing the HRF array values here_:
 INVOKE              OutputStr, ADDR HRFValues
 INVOKE              OutputStr, ADDR OpenBracket
 mov                 ecx, 0
-_OutputHRFArray:
-	lea				ebx,HRF 					;Calculate the memory array offset using the formulae: (ecx * 4) + baseaddress
+DisplayHRFArray:
+	lea				ebx,HRF 				   ;Calculate the memory array offset using the formulae: (ecx * 4) + baseaddress
 	imul			eax,ecx,4                  ;EAX = ECX*4
 	add				ebx,eax                    ;EBX = EBX + EAX, to move to the correct index in the array
-	INVOKE			OutputInt,[ebx]				;Output the array item at the index
+	INVOKE			OutputInt,[ebx]			   ;Output the array item at the index
 	cmp				ecx,2                      ;Comparing the values
-	je				_Comma
+	je				InsertComma
 	INVOKE			OutputStr, ADDR Comma
-_Comma:
+InsertComma:
 	inc				ecx
 	cmp 		   	ecx,3 
-	jl				_OutputHRFArray
+	jl				DisplayHRFArray
 	INVOKE			OutputStr, ADDR ClosedBracket
     INVOKE          OutputStr, ADDR EndLine
-    INVOKE          OutputStr, ADDR ContinueCode
+    INVOKE          OutputStr, ADDR Restart
 	INVOKE          InputInt
 	cmp             eax, 0
-	je              _Exit     
+	je              ExitProgram     
     jmp             _start
-_Exit:
-    ; Destroy the stack frame
+ExitProgram:
+    ;Destroy stack frame
 	mov				esp,ebp
 	pop				ebp
+    
+
 	INVOKE ExitProcess, 0
 Public _start
 END
